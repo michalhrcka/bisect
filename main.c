9abf4a9d5e3342863e6a4e2595e12a09a2e09cca
Michal Hrcka <michal.hrcka@autodesk.com>
2025-09-02 15:23:34 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int sum_to_n(int n) {
+    int s = 0;
+    for (int i = 1; i <= n; i++) {
+        s += i;
+    }
+    return s;
+}
+
+int main(int argc, char **argv) {
+    int n = 10;
+    if (argc > 1) n = atoi(argv[1]);
+    printf("%d\n", sum_to_n(n));
+    return 0;
+}
